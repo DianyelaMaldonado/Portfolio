c906651ffb04bc5cd4870dc223717822862f9728
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2025-01-30 00:15:02 -0600

    First Commit, Main Hero was Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { MainHero } from "./components/MainHero";
+
+function App() {
+  return (
+    <>
+      <MainHero />
+    </>
+  );
+}
+
+export default App;
